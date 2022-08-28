a8b80336d79fe6fb4525ec92424ee29b1d6b3278
George Grunfelder <68717396+GitGrunfelder@users.noreply.github.com>
2022-08-28 19:28:48 -0400

    Separated some problems into new files.

--- coin_flip_streaks.py
+++ coin_flip_streaks.py
@@ -1,14 +1,5 @@
 import random
 
-# def list_to_string(list):
-#     return ', '.join(list[:-1]) + ", and " + list[-1] # This function returns a joined list, using the delimiter specified
-#                                                       # before the method. It goes until the last item, and concatenates
-#                                                       # the item after ', and'. Will work on all lengths of list.
-
-# list1 = ['apples', 'bananas', 'tofu', 'cats']
-# print(list_to_string(list1))
-#______________________________________________________________________________________________________________________________________
-
 def flip_lists():
     
     h_t_list = []
