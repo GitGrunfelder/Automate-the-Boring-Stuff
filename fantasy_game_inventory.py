dceef64a8651290b63a1fcbd2e5fae2d61bccf5f
George Grunfelder <68717396+GitGrunfelder@users.noreply.github.com>
2022-09-11 10:48:27 -0400

    Created an inventory program.

---
+++ fantasy_game_inventory.py
@@ -0,0 +1,24 @@
+# Takes a given inventory and returns a message
+# of # of each and total items in inventory.
+
+inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def displayInventory(inventory):
+    print('Inventory:')
+    item_total = 0
+    for k, v in inventory.items():
+        print(f'{v} {k}')
+        item_total += v
+    print(f'Total number of items: {item_total}')
+    
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] = inventory[item] + 1
+    return inventory
+    
+displayInventory(inv)
+inv = addToInventory(inv, dragonLoot)
+print('\nAnd below we find the updated inventory!\n')
+displayInventory(inv)
